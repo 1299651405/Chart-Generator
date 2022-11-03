310aff115404452b5148daaeed10846c3a3d2ece
Conor <1299651405@mail.com>
2022-11-03 17:46:29 +0000

    new

--- index.js
+++ index.js
@@ -3,21 +3,19 @@ const localFile = document.getElementById('input');
 localFile.addEventListener("change", handleFiles, false);
 
 function handleFiles() {
-    const fileList = this.files;
-    var file = fileList[0];
+    const file = this.files[0];
+
     const reader = new FileReader();
     reader.onload = function (e) {
-        console.log(e.target.result);
-        var data = d3.csvParse(e.target.result);
-        chart(data);
-
-
+        gen_chart(d3.csvParse(e.target.result));
     };
     reader.readAsText(file);
 }
 
-const example = d3.csvParse("example.csv");
-const example_with_groups = d3.csvParse("example_with_groups.csv");
+function example() {
+    d3.csv('example.csv').then(function (data) { gen_chart(data); })
+}
+
 
 
 // set the dimensions and margins of the graph
@@ -28,20 +26,21 @@ const margin = { top: 10, right: 10, bottom: 10, left: 10 },
     outerRadius = Math.min(width, height) / 4;   // the outerRadius goes from the middle of the SVG area to the border
 
 // append the svg object to the body of the page
-var svg = d3.select("#my_dataviz")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", `translate(${width / 2},${height / 2})`); // Add 100 on Y translation, cause upper bars are longer
 
 function gen_chart(data) {
-    d3.selectAll("svg > *").remove();
+    d3.selectAll("svg").remove();
+
+    var svg = d3.select("#chart")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${width / 2},${height / 2})`); // Add 100 on Y translation, cause upper bars
     // X scale
     const x = d3.scaleBand()
         .range([0, 2 * Math.PI])    // X axis goes from 0 to 2pi = all around the circle. If I stop at 1Pi, it will be around a half circle
         .align(0)                  // This does nothing ?
-        .domain(data.map(d => d.Subtopic)); // The domain of the X axis is the list of states.
+        .domain(data.map(d => d.Category)); // The domain of the X axis is the list of states.
 
     // Y scale
     const y = d3.scaleLinear()
@@ -58,8 +57,8 @@ function gen_chart(data) {
         .attr("d", d3.arc()     // imagine your doing a part of a donut plot
             .innerRadius(innerRadius)
             .outerRadius(d => y(d['Score']))
-            .startAngle(d => x(d.Subtopic))
-            .endAngle(d => x(d.Subtopic) + x.bandwidth())
+            .startAngle(d => x(d.Category))
+            .endAngle(d => x(d.Category) + x.bandwidth())
             .padAngle(0.00)
             .padRadius(innerRadius));
 
@@ -78,11 +77,11 @@ function gen_chart(data) {
         .data(data)
         .enter()
         .append("g")
-        .attr("text-anchor", function (d) { return (x(d.Subtopic) + x.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "end" : "start"; })
-        .attr("transform", function (d) { return "rotate(" + ((x(d.Subtopic) + x.bandwidth() / 2) * 180 / Math.PI - 90) + ")" + "translate(" + (outerRadius + 10) + ",0)"; })
+        .attr("text-anchor", function (d) { return (x(d.Category) + x.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "end" : "start"; })
+        .attr("transform", function (d) { return "rotate(" + ((x(d.Category) + x.bandwidth() / 2) * 180 / Math.PI - 90) + ")" + "translate(" + (outerRadius + 10) + ",0)"; })
         .append("text")
-        .text(function (d) { return (d.Subtopic) })
-        .attr("transform", function (d) { return (x(d.Subtopic) + x.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "rotate(180)" : "rotate(0)"; })
+        .text(function (d) { return (d.Category) })
+        .attr("transform", function (d) { return (x(d.Category) + x.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "rotate(180)" : "rotate(0)"; })
         .style("font-size", "11px")
         .attr("dominant-baseline", "central");
 
